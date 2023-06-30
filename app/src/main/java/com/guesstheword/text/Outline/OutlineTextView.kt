0fb63638a557b16f811e9b810ce5e9ff63999072
RskullW <mailcom.99@mail.ru>
2023-06-30 15:48:38 +0300

    first commit

---
+++ app/src/main/java/com/guesstheword/text/Outline/OutlineTextView.kt
@@ -0,0 +1,37 @@
+package com.guesstheword.text.Outline
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class OutlineTextView(context: Context, attrs: AttributeSet?) : AppCompatTextView(context, attrs) {
+    private val outlineColor = Color.BLACK
+    private val outlineWidth = 5.0f
+
+    override fun onDraw(canvas: Canvas) {
+        val textColor = currentTextColor
+
+        val paint = Paint()
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = outlineWidth
+        paint.color = outlineColor
+        paint.textSize = textSize
+        paint.typeface = typeface
+        paint.textAlign = Paint.Align.CENTER
+
+        val text = text.toString()
+
+        val x = width / 2.0f
+        val y = (height / 2.0f) - ((paint.descent() + paint.ascent()) / 2.0f)
+
+        canvas.drawText(text, x, y, paint)
+
+        paint.style = Paint.Style.FILL
+        paint.color = textColor
+        canvas.drawText(text, x, y, paint)
+    }
+}
