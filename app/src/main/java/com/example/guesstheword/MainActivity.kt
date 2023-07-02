eb99bf8175d7986f41dd313bf93c070c83f5f2e2
RskullW <mailcom.99@mail.ru>
2023-07-02 21:17:06 +0300

    feat: added button appearance animation based on user category and mode selection
    Implemented an animation for the 'Start Game' button to smoothly appear when the user selects a category and mode, and vice versa.

--- app/src/main/java/com/example/guesstheword/MainActivity.kt
+++ app/src/main/java/com/example/guesstheword/MainActivity.kt
@@ -1,33 +1,40 @@
 package com.example.guesstheword
 
-import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.print.PrintAttributes.Margins
+import android.util.TypedValue
 import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.LinearLayout
-import androidx.core.view.marginLeft
-import androidx.core.view.marginRight
-import androidx.core.view.marginTop
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonReady: Button
     private lateinit var buttonRandomWord: Button
     private var isButtonPressed: Boolean = false
     private val colorTextButtonPressed: Int = R.color.colorTextButtonPressed
     private val colorTextButtonNotPressed: Int = R.color.colorTextButtonNotPressed
     private var mapButtonMode: MutableMap<String, ButtonState> = mutableMapOf()
     private var mapButtonCategories: MutableMap<String, ButtonState> = mutableMapOf()
+    private val isSelectMode: MutableLiveData<Boolean> = MutableLiveData(false)
+    private val isSelectCategory: MutableLiveData<Boolean> = MutableLiveData(false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
         initializeButtonsMode()
         initializeButtonsCategories()
+        initializeButtonReady()
     }
 
     private fun initializeButtonsMode() {
-
         val buttonIds = listOf(
             R.id.buttonRandomWord1,
             R.id.buttonRandomWord2,
@@ -53,12 +60,13 @@ class MainActivity : AppCompatActivity() {
                     if (button == buttonState) continue
                     button.value.isPressed = false
                 }
+
+                isSelectMode.value = mapButtonMode.checkState()
+
             }
         }
     }
-
     private fun initializeButtonsCategories() {
-
         val buttonContainer = findViewById<LinearLayout>(R.id.buttonContainer)
 
         var index: Int = 0
@@ -112,9 +120,38 @@ class MainActivity : AppCompatActivity() {
                     if (button == buttonState) continue
                     button.value.isPressed = false
                 }
+
+                isSelectCategory.value = mapButtonCategories.checkState()
             }
         }
     }
+    private fun initializeButtonReady() {
+        buttonReady = findViewById(R.id.buttonStartGame)
+
+        updateButtonVisibility()
+
+        isSelectMode.observe(this, Observer { updateButtonVisibility() })
+        isSelectCategory.observe(this, Observer { updateButtonVisibility() })
+    }
+
+    private fun updateButtonVisibility() {
+        if (isSelectMode.value == true && isSelectCategory.value == true) {
+            buttonReady.visibility = if (buttonReady.visibility != View.VISIBLE) {
+                val anim = AnimationUtils.loadAnimation(this, R.anim.button_appear)
+                buttonReady.startAnimation(anim)
+                View.VISIBLE
+            } else buttonReady.visibility
+
+        } else {
+            buttonReady.visibility = if (buttonReady.visibility != View.GONE) {
+
+                val anim = AnimationUtils.loadAnimation(this, R.anim.button_disappears)
+                buttonReady.startAnimation(anim)
+                View.GONE
+            } else buttonReady.visibility
+        }
+    }
+
     private fun createNewLinearLine(index: Int):LinearLayout? {
         if (index % 2 == 0) {
             val linearLayout = LinearLayout(this)
@@ -129,9 +166,17 @@ class MainActivity : AppCompatActivity() {
 
         return null
     }
-
     private fun Int.dpToPx(): Int {
         val scale = resources.displayMetrics.density
         return (this * scale + 0.5f).toInt()
+    }
+    private fun Map<String, ButtonState>.checkState(): Boolean {
+        for (value in values) {
+            if (value.isPressed) {
+                return true
+            }
+        }
+
+        return false
     }
 }
